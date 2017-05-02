510299864357ea5790065e2d047b8cf387c7cac0
Raswanson01 <raswanson01@gmail.com>
2017-05-01 22:10:22 -0500

    Update Bullet.js

--- Bullet.js
+++ Bullet.js
@@ -1,15 +1,18 @@
-function Bullet(spd)
-{
-    if (Game.RUNNING == currentGameState) {
-        image = preload.getResult("ShotBlue");
-        var bullet = new createjs.Bitmap(image);
-        bullet.x = player.x + 100; bullet.y = player.y + 35;
-        stage.addChild(bullet);
-        bullets.push(bullet);
-        if (laser != null) {
-            laser.stop();
-        }
-        laser = createjs.Sound.play('Laser', createjs.Sound.INTERRUPT_NONE, 0, 0, 0, .5, 0);
-        this.speed = spd;
-    }
-}
+ var Bullet = {
+    newBullet:function(spd)
+    {
+        if (Game.RUNNING == currentGameState) {
+            image = preload.getResult("ShotBlue");
+            var bullet = new createjs.Bitmap(image);
+            bullet.x = player.x + 100; bullet.y = player.y + 35;
+            stage.addChild(bullet);
+            bullets.push(bullet);
+            if (laser != null) {
+                laser.stop();
+            }
+            var laser = createjs.Sound.play('Laser', createjs.Sound.INTERRUPT_NONE, 0, 0, 0, .5, 0);
+     }
+},
+    speed:0,
+    setSpeed:function(num) {this.speed = num; }
+};
