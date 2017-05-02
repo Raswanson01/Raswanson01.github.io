5160ff3fb402200896e744858bf829fb70a66693
Raswanson01 <raswanson01@gmail.com>
2017-05-02 00:37:39 -0500

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,5 +1,5 @@
-var player = {
-    Player:function(initX, initY)
+var Player = {
+    Player:function(initX, initY, dy)
     {
         var pShip = new createjs.Container();
 
@@ -10,12 +10,36 @@ var player = {
         var playerImage = new createjs.Bitmap(preload.getResult("PlayerShip"));
         pShip.addChild(playerImage);
         
-        return pShip;
+        this.x = initX;
+        this.y = initY;
+        this.dY = dy;
+        return pShip;       
     },   
-    x:75, 
-    y:300,
-    rFire:false,
-    dY:15,
-    move:function () { this.y += this.dY; },
-    switchRapidFire:function() { player.rFire = !player.rFire; }
+    
+    movePlayer:function()
+    {
+        if (upKey)
+        {
+            if (this.y + this.dY <= 0) 
+                player.y = 0;
+            else 
+                this.y = this.y - this.dY;
+        }
+        else if (downKey)
+        {
+            if (this.y - this.dY >= 600) 
+                this.y = 600;
+            else 
+                this.y = this.y + this.dY;       
+        }
+    },
+    
+    createBullet:function()
+    {
+        image = preload.getResult("ShotBlue");
+        var bullet = new createjs.Bitmap(image);
+        bullet.x = this.x + 120; bullet.y = this.y + 37;
+        stage.addChild(bullet);
+        bullets.push(bullet);
+    }
 }
