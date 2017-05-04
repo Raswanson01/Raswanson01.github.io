6e7debb322b178bacfc91422ec29edeb1dfb8385
Raswanson01 <raswanson01@gmail.com>
2017-05-04 11:42:45 -0500

    Update Bullet.js

--- Bullet.js
+++ Bullet.js
@@ -1,11 +1,19 @@
  var Bullet = {
      moveShot:function()
      {
-        for(i = 0; i < bullets.length; i++)
-        {
-            if (bullets[i].x > 1200)
-                stage.removeChild(bullets[i]);
-            bullets[i].x += 15;
-        }
-     }
+       for(i = 0; i < bullets.length; i++)
+       {
+           if (bullets[i].x > 1200)
+               stage.removeChild(bullets[i]);
+           bullets[i].x += 15;
+       }
+    },
+    createBullet:function()
+    {
+        image = preload.getResult("ShotBlue");
+        var bullet = new createjs.Bitmap(image);
+        bullet.x = player.x + 120; bullet.y = player.y + 37;
+        stage.addChild(bullet);
+        bullets.push(bullet);
+    }
 };
